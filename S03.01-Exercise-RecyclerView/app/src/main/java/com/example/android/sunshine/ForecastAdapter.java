31bc484ffe87bc0bf47e111bcc9db72435d74e29
btgn <tbakula@calstatela.edu>
2017-06-26 16:09:51 -0700

    Lab - 3 S03.01

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,60 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by btril on 06/24/17.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+    private String[] mWeatherData;
+
+    public ForecastAdapter(){
+
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            this.mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int idForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(idForListItem, parent, false);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        String weatherForThisDay = mWeatherData[position];
+        holder.mWeatherTextView.setText(weatherForThisDay);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWeatherData == null)
+            return 0;
+        else
+            return mWeatherData.length;
+    }
+
+    public void setmWeatherData(String[] mWeatherData) {
+        this.mWeatherData = mWeatherData;
+        notifyDataSetChanged();
+    }
+}
